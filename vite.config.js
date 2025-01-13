7d360e3537efab4f6aabdaaecaac5b2e7b0ed05b
Mohammed Amir Hamza <mohammedamir.hamza@stonybrook.edu>
2025-01-13 18:15:22 -0500

    SSL Certificate Creation added

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
 
 export default defineConfig(({ command, mode }) => {
     return {
@@ -6,6 +9,14 @@ export default defineConfig(({ command, mode }) => {
             alias: {
                 'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
             }
+        },
+        server: {
+            https: {
+                key: fs.readFileSync(path.resolve(__dirname, 'localhost+2-key.pem')),
+                cert: fs.readFileSync(path.resolve(__dirname, 'localhost+2.pem'))
+            },
+            open: true, // Automatically open the app in the browser
         }
+
     };
 });
